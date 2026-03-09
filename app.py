03817a8dd26caa194be6bfab3a5b01ca8cd04aad
rogers8733-rgb <rogers8733@gmail.com>
2026-03-08 22:11:04 -0500

    app file fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect
 import csv
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 FILE = "accounts.csv"
 FIELDS = ["name","address","city","classification"]
