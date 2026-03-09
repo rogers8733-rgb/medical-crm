a0339d345ffbbea751067c67fcb6e9d0fc9d8ed9
rogers8733-rgb <rogers8733@gmail.com>
2026-03-08 22:18:23 -0500

    add import

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-FILE = "accounts.csv"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE = os.path.join(BASE_DIR, "accounts.csv")
 FIELDS = ["name","address","city","classification"]
 
 def init_file():
@@ -58,7 +59,28 @@ def add_account():
     return render_template("add.html")
 
 
+@app.route("/import",methods=["GET","POST"])
+def import_csv():
+
+    if request.method=="POST":
+
+        file=request.files["file"]
+
+        if file:
+            stream=file.stream.read().decode("UTF8").splitlines()
+            reader=csv.reader(stream)
+
+            rows=list(reader)
+
+            with open(FILE,"w",newline="") as f:
+                writer=csv.writer(f)
+                writer.writerows(rows)
+
+        return redirect("/")
+
+    return render_template("import.html")
+
+
 if __name__=="__main__":
-    import os
     port=int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0",port=port)
